0d4e0dc5996946c55e545a8e99221e465ca3ba00
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-06-15 14:47:26 +0800

    Add model and controller for group

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Start seed the database!"
+(1..5).each do |i|
+  g = Group.new(title: "fake title #{i}", description: "fake description #{i}")
+  g.save!
+end
