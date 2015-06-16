135a7d474e856c94da1b87ca392a46765b77b262
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-06-16 17:12:44 +0800

    Fix bug that posts didn't be removed when related group removed

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :title, presence: true
   validates :description, length: { minimum: 5, maximum: 200 }
 end
