5dfd9ad01387740e37ff9916bebbca7c561ad030
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-06-23 17:06:58 +0800

    Add scope recent to show posts on recent order

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,6 +2,6 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.includes(:group).where(user_id: current_user)
+    @posts = Post.includes(:group).where(user_id: current_user).recent
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :group, counter_cache: true
   belongs_to :user
   validates :content, presence: true
+  scope :recent, -> { order("updated_at DESC") }
 end
